483cf801dd5de661f5e9e23e1024435655dea672
Jonathan <jonathanmoorevla@gmail.com>
2022-06-16 18:16:34 -0500

    commit tree 95e47d68bbe1ab8568250232e0085c6a6891562a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const people = ['Guadalupe', 'Ollie', 'Aki'] 
+
+function writeCards(peopleList, event) {
+    let messageArray = []; // creates empty array to populate. outside for loop to avoid resetting as empty each iteration
+    for (let i = 0; i < peopleList.length; i++) {
+        messageArray.push(`Thank you, ${peopleList[i]}, for the wonderful ${event} gift!`);
+        // pushes each message to array as it iterates over the length of the names list
+    }
+    return messageArray; // returns array with all messages pushed to it
+}
+
+// console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'Birthday'))
+
+function countDown(startNum) {
+    for (let i = startNum; i >= 0; i--) {
+        console.log(i);
+    }
+}
